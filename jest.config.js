c727ae4320ec421b7529fb50727b2205665b2b16
Stuart Kennedy <rav2040@gmail.com>
2020-03-24 16:57:01 +1300

    Update config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,14 @@ module.exports = {
   testEnvironment: 'node',
   testPathIgnorePatterns: [
     '/node_modules/',
+    '__tests__/example'
   ],
-  preset: 'ts-jest',
-  verbose: true,
+  preset: 'ts-jest/presets/js-with-ts',
+  globals: {
+    'ts-jest': {
+      tsConfig: {
+        allowJs: true,
+      }
+    }
+  }
 };
