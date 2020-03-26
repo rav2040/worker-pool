d4547e2ff19fea85de04b7418efb231acb1c5b64
Stuart Kennedy <rav2040@gmail.com>
2020-03-26 20:15:07 +1300

    Add error handling

--- src/worker-script.ts
+++ src/worker-script.ts
@@ -1,28 +1,43 @@
-import { isMainThread, parentPort, workerData } from 'worker_threads';
+import { isMainThread, parentPort, workerData as activeTasksDataView } from 'worker_threads';
 
-const tasks: { [key: string]: (...args: any[]) => any } = {};
+const tasks: Map<string, (...args: any[]) => any> = new Map();
 
-export function createTask(name: string, task: (...args: any[]) => any) {
-  tasks[name] = task;
+/** Adds a callback function to the task map. */
+export function createTask(name: string, callback: (...args: any[]) => any): void {
+  tasks.set(name, callback);
 };
 
 // Code coverage doesn't work for the following code because it doesn't run in the main thread, so we ignore it.
 /* istanbul ignore if */
 if (!isMainThread) {
-  const idle = workerData;
-  idle.set([1]);
-
+  // The module has been imported by a worker.
   parentPort!.on('message', async (jobs) => {
-    idle.set([0]);
+    // Set the number of active tasks for this worker.
+    activeTasksDataView.set([jobs.length]);
 
+    // Iterate through the list of jobs and execute their corresponding callbacks, passing any given arguments.
     for (let i = 0; i < jobs.length; i++) {
       const { num, name, args } = jobs[i];
-      const runTask = tasks[name];
-      const result = await runTask(...args);
-      jobs[i] = { num, result };
+      const callback = tasks.get(name);
+
+      try {
+        if (callback === undefined) {
+          throw Error(`Task with name '${name}' was not found.`);
+        }
+
+        const result = await callback(...args);
+        jobs[i] = { num, result };
+      }
+
+      catch (err) {
+        jobs[i] = { num, err };
+      }
     }
 
+    // Pass the task results back to the main thread.
     parentPort!.postMessage(jobs);
-    idle.set([1]);
+
+    // Reset the number of active tasks for this worker to zero.
+    activeTasksDataView.set([0]);
   });
 }
