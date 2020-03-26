5529f5bc575a8d1305220f0a38c9075229a38aa0
Stuart Kennedy <rav2040@gmail.com>
2020-03-26 20:17:46 +1300

    Add comments

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,5 +1,10 @@
 const START = 0;
 
+/**
+ * Returns an anonymous function that returns the next iteration of a number, starting with zero, each time it's
+ * called. It does this up until it reaches the provided 'end' argument, at which point it resets to zero.
+ */
+
 export function createRepeatingSequence(end: number) {
   let n = START;
 
