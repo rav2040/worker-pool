3eff1075d4edd4a05dfb923ce3049344e796d9ee
Stuart Kennedy <rav2040@gmail.com>
2020-03-26 20:18:23 +1300

    Refactor code and add comments

--- src/worker-pool.ts
+++ src/worker-pool.ts
@@ -16,7 +16,13 @@ type WorkerJob = {
 
 type WorkerJobResult = {
   num: number,
-  result: any,
+  err?: Error,
+  result?: any,
+}
+
+type PromiseCallback = {
+  resolve: (value?: any) => void,
+  reject: (err: Error) => void,
 }
 
 const DEFAULT_DESTROYED = false;
@@ -26,16 +32,26 @@ const defaultNumWorkers = defaultMaxNumWorkers - 1;
 const defaultMaxQueueSize = Number.MAX_SAFE_INTEGER;
 const defaultMaxJobs = Number.MAX_SAFE_INTEGER;
 
-const view: unique symbol = Symbol('data view');
+const activeTasksDataView: unique symbol = Symbol('data view');
+
+/**
+ * The extended Worker class has an additional 'activeTasksDataView' property that must be passed as the
+ * second argument when the instance is created. This is an Int32Array which references a SharedArrayBuffer, used by the
+ * worker thread to report the number of active tasks it is currently executing back to the worker pool.
+ */
 
 class Worker extends WorkerBase {
-  [view]:Uint8Array;
+  [activeTasksDataView]: Int32Array;
 
-  constructor(filename: string, options: any, dataView: Uint8Array) {
-    super(filename, options);
-    this[view] = dataView;
+  constructor(filename: string, dataView: Int32Array) {
+    super(filename, { workerData: dataView });
+    this[activeTasksDataView] = dataView;
   }
 }
+
+/**
+ * The WorkerPool class creates and manages a pool of Workers which can execute tasks included in the provided script.
+ */
 
 export class WorkerPool {
   #destroyed = DEFAULT_DESTROYED;
@@ -43,15 +59,12 @@ export class WorkerPool {
   private readonly _numWorkers: number;
   private readonly _maxQueueSize: number;
   private readonly _maxJobsPerWorker: number;
-  private readonly _seq: () => number;
+  private readonly _seq: () => number; // Returns a number that iterates by 1 each time it's called.
 
   private readonly _workers: Worker[] = [];
   private readonly _timeouts: NodeJS.Timeout[] = [];
   private readonly _queue: WorkerJob[] = [];
-  private readonly _callbacks: { [key: number]: (value: any) => void } = {};
-
-  private _activeTasks: number = 0;
-  private _pendingTasks: number = 0;
+  private readonly _callbacks: Map<number, PromiseCallback> = new Map();
 
   get isDestroyed() {
     return this.#destroyed;
@@ -69,19 +82,25 @@ export class WorkerPool {
     return this._maxJobsPerWorker;
   }
 
-  get activeTasks() {
-    return this._activeTasks;
+  get pendingTasks() {
+    return this._queue.length;
   }
 
-  get pendingTasks() {
-    return this._pendingTasks;
+  get activeTasks() {
+    let i, n;
+
+    for (i = 0, n = 0; i < this._numWorkers; i++) {
+      n += this._workers[i][activeTasksDataView][0];
+    }
+
+    return n;
   }
 
   get numIdleWorkers() {
     let i, n;
 
     for (i = 0, n = 0; i < this._numWorkers; i++) {
-      n = n + (this._workers[i][view][0] === 1 ? 1 : 0);
+      n = n + (this._workers[i][activeTasksDataView][0] === 0 ? 1 : 0);
     }
 
     return n;
@@ -109,10 +128,15 @@ export class WorkerPool {
     this._seq = createRepeatingSequence(this._maxQueueSize);
 
     for (let i = 0; i < this._numWorkers; i++) {
-      const buf = new SharedArrayBuffer(1);
-      const view = new Uint8Array(buf, 0, 1);
-      const worker = new Worker(filename, { workerData: view }, view);
+      // A SharedArrayBuffer is passed to the worker, which is used to report how many active tasks it has.
+      const buf = new SharedArrayBuffer(4);
+      const view = new Int32Array(buf);
 
+      // Create a worker with the provided script.
+      const worker = new Worker(filename, view);
+
+      // A recursive function executed with setTimeout(). Removes any jobs currently in the queue (up until the max jobs
+      // limit) and passes them to the worker thread.
       const processJobs = () => {
         const jobs = this._queue.splice(0, this._maxJobsPerWorker);
 
@@ -125,27 +149,33 @@ export class WorkerPool {
           this._timeouts[i] = setTimeout(processJobs, 0);
         });
 
-        this._activeTasks += jobs.length;
-        this._pendingTasks -= jobs.length;
-
         worker.postMessage(jobs);
       };
 
       this._timeouts[i] = setTimeout(processJobs, 0);
 
+      // When task results are received from the worker, iterate over them and execute their corresponding callbacks.
       worker.on('message', (results: WorkerJobResult[]) => {
         for (let i = 0; i < results.length; i++) {
-          const { num, result } = results[i];
-          const callback = this._callbacks[num];
-          callback(result);
-        }
+          const { num, err, result } = results[i];
+          const { resolve, reject } = this._callbacks.get(num)!;
 
-        this._activeTasks -= results.length;
+          err ? reject(err) : resolve(result);
+
+          // Delete redundant callbacks to avoid running out of memory.
+          this._callbacks.delete(num);
+        }
       });
 
+      // Add the newly created worker to the worker pool.
       this._workers.push(worker);
     }
   }
+
+  /**
+   * Execute a task by name. All arguments after the first argument are passed on to the worker. Returns
+   * a promise which either resolves to the result of the task callback, or rejects with an Error.
+   */
 
   exec(name: string, ...args: any[]) {
     return new Promise<any>((resolve, reject) => {
@@ -154,7 +184,6 @@ export class WorkerPool {
       if (this.#destroyed) {
         err = Error('The worker pool has been destroyed.');
       }
-
 
       else if (this._queue.length >= this._maxQueueSize) {
         err = Error(`Max job queue size has been reached: ${this._maxQueueSize} jobs`);
@@ -166,11 +195,17 @@ export class WorkerPool {
       }
 
       const num = this._seq();
-      this._callbacks[num] = resolve;
+      this._callbacks.set(num, { resolve, reject });
       this._queue.push({ num, name, args });
-      this._pendingTasks++;
     });
   }
+
+  /**
+   * Destroys the worker pool instance by terminating all workers, preventing the worker pool from being used again.
+   * Tasks not yet completed are immediately canceled. This should be called when the worker pool is no longer needed
+   * in order to prevent open handles. Returns a promise that resolves once all workers have been successfully
+   * terminated.
+   */
 
   destroy() {
     return new Promise<void>((resolve) => {
